a7b736da9721b3eaa35a141843790fbb3c1d0b2d
Moritz Jacobs <mail@moritzjacobs.de>
2014-01-28 12:40:17 +0100

    Enhance healing information distribution by channeling energy

--- potentise.sh
+++ potentise.sh
@@ -4,9 +4,15 @@ ITERATIONS=${2:-200}
 
 for ((n = 1; n <= $ITERATIONS; n++)); do
 	# shake
-	awk NR==$((${RANDOM} % `wc -l < $1` + 1)) $SOURCE_FILE > solution.txt
+	cat $SOURCE_FILE | while IFS= read -r line
+	do
+		printf "%06d %s\n" $RANDOM "$line" > solution.txt
+	done | sort -n | cut -c8-
 
 	# dilute
+		
+	awk NR==$((${RANDOM} % `wc -l < $1` + 1)) $SOURCE_FILE > solution.txt
+
 	for i in {1..99}
 	do
 	   echo -e >> solution.txt
